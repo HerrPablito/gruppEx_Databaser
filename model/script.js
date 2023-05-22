867fb5f1bfb10b15bf4cdbd2486b9e154738f6cf
MartinJJScript <zerthas@gmail.com>
2023-05-22 10:50:41 +0200

    subs_id fixat

--- model/script.js
+++ model/script.js
@@ -4,6 +4,6 @@ const db = createDbConnection();
 
 // db.run(`INSERT INTO users VALUES ("x5", "Mart", "pwd123") `)
  
-// db.run(`INSERT INTO subscriptions VALUES ("x1", "2") `)
+//db.run(`INSERT INTO subscriptions (user_id, channel_name) VALUES ("x1", "2")`)
 
-// db.run(`DELETE FROM users WHERE user_name = "p" `)
+//db.run(`DELETE FROM subscriptions WHERE subs_id > 3 `)
